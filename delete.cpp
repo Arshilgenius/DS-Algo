5f7ed4262126efcd5e4070f5506544aebe3b2a5b
Arshil Khan <arshil.khan@innovaccer.com>
2021-10-03 23:48:20 +0530

    deletion

---
+++ delete.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    // Allocate Heap memory
+    int* array = new int[10];
+     
+    // Deallocate Heap memory
+    delete[] array;
+ 
+    return 0;
+}
